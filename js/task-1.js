15630483f06882051e36659eb353188c6f070936
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-01-28 01:12:29 +0200

    Update Readme.md

---
+++ js/task-1.js
@@ -0,0 +1,35 @@
+const categories = document.getElementsByClassName('item');
+
+console.log(`Number of categories: ${categories.length}`);
+
+
+
+
+
+
+
+
+// const category = document.getElementsByTagName('h2');
+
+// console.log(category);
+
+
+
+// Number of categories: 3
+// Category: Animals
+// Elements: 4
+// Category: Products
+// Elements: 3
+// Category: Technologies
+// Elements: 5
+
+
+
+
+
+
+// const numbers = categories;
+
+// numbers.forEach(function(number) {
+//   console.log(number);
+// });
